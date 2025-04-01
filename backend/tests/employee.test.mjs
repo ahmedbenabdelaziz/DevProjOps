9ec71becfc104c7e73218f3cc9240dfe022d2368
ahmedbenabdelaziz <129238060+ahmedbenabdelaziz@users.noreply.github.com>
2025-04-02 00:20:18 +0100

    Update employee.test.mjs

--- backend/tests/employee.test.mjs
+++ backend/tests/employee.test.mjs
@@ -1,5 +1,6 @@
-describe('Sample Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
+// __tests__/sum.test.js
+const sum = (a, b) => a + b;
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
